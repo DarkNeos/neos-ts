e1d53625adafcb477d9f04d75c6b1bdf939155bc
SKTT1Ryze <linuxgnulover@gmail.com>
2022-08-13 11:50:00 +0800

    remove logo.svg and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to neos-ts!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DarkNeos"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DarkNeos
         </a>
       </header>
     </div>
