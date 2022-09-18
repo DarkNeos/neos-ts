d2df30c246c9ac30887d536682ac928ee6f7d900
SKTT1Ryze <linuxgnulover@gmail.com>
2022-09-18 19:28:11 +0800

    ported react-router6

---
+++ src/WaitRoom.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+
+export default function WaitRoom() {
+  const params = useParams<{
+    player: string,
+    passWd: string,
+    ip: string
+  }>();
+
+  return (<div>
+    <p>player: {params.player}</p>
+    <p>passwd: {params.passWd}</p>
+    <p>ip: {params.ip}</p>
+  </div>);
+}
