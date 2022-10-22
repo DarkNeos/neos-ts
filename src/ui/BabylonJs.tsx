8b40ef3dbc37b07db52fb200244d3b64f83a1fe3
SKTT1Ryze <linuxgnulover@gmail.com>
2022-10-22 13:08:03 +0800

    fix lint

--- src/ui/BabylonJs.tsx
+++ src/ui/BabylonJs.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import * as BABYLON from "@babylonjs/core";
 
-export default function() {
+export default function () {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -10,17 +10,33 @@ export default function() {
       const engine = new BABYLON.Engine(canvasCurrent, true);
       const scene = new BABYLON.Scene(engine);
 
-      const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+      const camera = new BABYLON.FreeCamera(
+        "camera1",
+        new BABYLON.Vector3(0, 5, -10),
+        scene
+      );
       camera.setTarget(BABYLON.Vector3.Zero());
       camera.attachControl(canvasCurrent, true);
 
-      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+      const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+      );
       light.intensity = 0.7;
 
-      const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+      const sphere = BABYLON.MeshBuilder.CreateSphere(
+        "sphere",
+        { diameter: 2, segments: 32 },
+        scene
+      );
       sphere.position.y = 1;
 
-      const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+      const ground = BABYLON.MeshBuilder.CreateGround(
+        "ground",
+        { width: 6, height: 6 },
+        scene
+      );
 
       engine.runRenderLoop(() => {
         scene.render();
