eeeeb3f91143795a504755b3b4c9b9650e5ccd99
SKTT1Ryze <linuxgnulover@gmail.com>
2022-11-29 20:56:08 +0800

    add observeStore

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 /*
  * 全局状态存储模块
  * */
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Unsubscribe } from "@reduxjs/toolkit";
 import joinedReducer from "./reducers/joinSlice";
 import chatReducer from "./reducers/chatSlice";
 import playerReducer from "./reducers/playerSlice";
@@ -17,6 +17,26 @@ export const store = configureStore({
     duel: duelReducer,
   },
 });
+
+// Ref: https://github.com/reduxjs/redux/issues/303
+export function observeStore<T>(
+  select: (state: RootState) => T,
+  onChange: (prev: T | null, cur: T) => void
+): Unsubscribe {
+  let currentState: T | null = null;
+
+  const changeHook = () => {
+    const nextState = select(store.getState());
+    if (nextState !== currentState) {
+      onChange(currentState, nextState);
+      currentState = nextState;
+    }
+  };
+
+  const unsubscribe = store.subscribe(changeHook);
+  changeHook();
+  return unsubscribe;
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
