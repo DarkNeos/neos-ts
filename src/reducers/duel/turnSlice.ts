e7bbc31785c3c5eec56e0c74803f9d0160499c5b
SKTT1Ryze <linuxgnulover@gmail.com>
2022-12-01 19:21:13 +0800

    finish new_turn

--- src/reducers/duel/turnSlice.ts
+++ src/reducers/duel/turnSlice.ts
@@ -1,4 +1,5 @@
 import { PayloadAction, CaseReducer } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
 import { DuelState } from "./mod";
 
 export const newTurnImpl: CaseReducer<DuelState, PayloadAction<number>> = (
@@ -7,3 +8,6 @@ export const newTurnImpl: CaseReducer<DuelState, PayloadAction<number>> = (
 ) => {
   state.currentPlayer = action.payload;
 };
+
+export const selectCurrentPlayer = (state: RootState) =>
+  state.duel.currentPlayer;
