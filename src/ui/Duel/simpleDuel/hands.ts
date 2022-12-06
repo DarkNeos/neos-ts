b0bdea90937e1afcdf5d6f0a20507393883369db
SKTT1Ryze <linuxgnulover@gmail.com>
2022-12-06 22:07:08 +0800

    add hover action

--- src/ui/Duel/simpleDuel/hands.ts
+++ src/ui/Duel/simpleDuel/hands.ts
@@ -40,5 +40,14 @@ export default (hands: Card[], scene: BABYLON.Scene) => {
         }
       )
     );
+    // 监听`Hover`事件
+    hand.actionManager.registerAction(
+      new BABYLON.ExecuteCodeAction(
+        BABYLON.ActionManager.OnPointerOverTrigger,
+        (event) => {
+          console.log(`<Hover>hand: ${idx}`, "card:", item, "event:", event);
+        }
+      )
+    );
   });
 };
