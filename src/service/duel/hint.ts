a4c32592fee25ffa6fb8a5aafdafe505d15f20a7
SKTT1Ryze <linuxgnulover@gmail.com>
2022-12-24 12:09:14 +0800

    update hint service

--- src/service/duel/hint.ts
+++ src/service/duel/hint.ts
@@ -1,10 +1,18 @@
 import { ygopro } from "../../api/ocgcore/idl/ocgcore";
 import { AppDispatch } from "../../store";
+import { fetchHintMeta } from "../../reducers/duel/hint";
+import MsgHint = ygopro.StocGameMessage.MsgHint;
 
-export default (
-  hint: ygopro.StocGameMessage.MsgHint,
-  dispatch: AppDispatch
-) => {
-  // TODO
-  console.log(hint);
+export default (hint: MsgHint, dispatch: AppDispatch) => {
+  const player = hint.player;
+  switch (hint.hint_type) {
+    case MsgHint.HintType.HINT_EVENT:
+    case MsgHint.HintType.HINT_MESSAGE: {
+      dispatch(fetchHintMeta([player, hint.hint_data]));
+      break;
+    }
+    default: {
+      console.log(`Unhandled hint type ${MsgHint.HintType[hint.hint_type]}`);
+    }
+  }
 };
