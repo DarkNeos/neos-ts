4b8c5cff9b861ccc5aad2de122c7820ef36cc27f
SKTT1Ryze <linuxgnulover@gmail.com>
2023-01-01 11:16:31 +0800

    move interactTypeToString to util.ts

--- src/ui/Duel/hands.tsx
+++ src/ui/Duel/hands.tsx
@@ -2,7 +2,7 @@ import * as BABYLON from "@babylonjs/core";
 import { useAppSelector } from "../../hook";
 import { selectMeHands } from "../../reducers/duel/handsSlice";
 import * as CONFIG from "../../config/ui";
-import { Hand, InteractType } from "../../reducers/duel/util";
+import { Hand } from "../../reducers/duel/util";
 import {
   setCardModalImgUrl,
   setCardModalIsOpen,
@@ -14,7 +14,7 @@ import { useHover } from "react-babylonjs";
 import { useClick } from "./hook";
 import { useState, useRef, useEffect } from "react";
 import { useSpring, animated } from "./spring";
-import { zip } from "./util";
+import { zip, interactTypeToString } from "./util";
 
 const groundShape = CONFIG.GroundShape();
 const left = -(groundShape.width / 2);
@@ -141,32 +141,6 @@ const CHand = (props: {
     </animated.transformNode>
   );
 };
-
-function interactTypeToString(t: InteractType): string {
-  switch (t) {
-    case InteractType.SUMMON: {
-      return "普通召唤";
-    }
-    case InteractType.SP_SUMMON: {
-      return "特殊召唤";
-    }
-    case InteractType.POS_CHANGE: {
-      return "改变表示形式";
-    }
-    case InteractType.MSET: {
-      return "前场放置";
-    }
-    case InteractType.SSET: {
-      return "后场放置";
-    }
-    case InteractType.ACTIVATE: {
-      return "发动效果";
-    }
-    default: {
-      return "未知选项";
-    }
-  }
-}
 
 const handPositons = (player: number, hands: Hand[]) => {
   const gap = groundShape.width / (hands.length - 1);
