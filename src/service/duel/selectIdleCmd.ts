2297be274925aa24956156c48d4073379a426ed8
chechunchi <chechunchi.ccc@bytedance.com>
2023-01-15 16:17:31 +0800

    update idleCmd service

--- src/service/duel/selectIdleCmd.ts
+++ src/service/duel/selectIdleCmd.ts
@@ -14,62 +14,69 @@ export default (selectIdleCmd: MsgSelectIdleCmd, dispatch: AppDispatch) => {
   // 先清掉之前的手牌互动性
   dispatch(clearHandsIdleInteractivity(player));
 
-  for (let cmd of cmds) {
-    let interactType;
-    switch (cmd.idle_type) {
-      case MsgSelectIdleCmd.IdleCmd.IdleType.SUMMON: {
-        interactType = InteractType.SUMMON;
-        break;
-      }
-      case MsgSelectIdleCmd.IdleCmd.IdleType.SPSUMMON: {
-        interactType = InteractType.SP_SUMMON;
-        break;
-      }
-      case MsgSelectIdleCmd.IdleCmd.IdleType.POS_CHANGE: {
-        interactType = InteractType.POS_CHANGE;
-        break;
-      }
-      case MsgSelectIdleCmd.IdleCmd.IdleType.MSET: {
-        interactType = InteractType.MSET;
-        break;
-      }
-      case MsgSelectIdleCmd.IdleCmd.IdleType.SSET: {
-        interactType = InteractType.SSET;
-        break;
-      }
-      case MsgSelectIdleCmd.IdleCmd.IdleType.ACTIVATE: {
-        interactType = InteractType.ACTIVATE;
-        break;
-      }
-    }
+  cmds.forEach((cmd) => {
+    const interactType = idleTypeToInteractType(cmd.idle_type);
 
-    for (let data of cmd.idle_datas) {
-      const card_info = data.card_info;
-      if (card_info.location === 2) {
-        // 目前只处理手牌场景
-        if (interactType === InteractType.ACTIVATE) {
-          // 发动效果会多一个字段
-          dispatch(
-            addHandsIdleInteractivity({
-              player,
-              sequence: card_info.sequence,
-              interactivity: {
-                interactType,
-                activateIndex: data.effect_description,
-                response: data.response,
-              },
-            })
-          );
-        } else if (interactType) {
-          dispatch(
-            addHandsIdleInteractivity({
-              player,
-              sequence: card_info.sequence,
-              interactivity: { interactType, response: data.response },
-            })
-          );
+    cmd.idle_datas.forEach((data) => {
+      const cardInfo = data.card_info;
+      switch (cardInfo.location) {
+        case ygopro.CardZone.HAND: {
+          if (interactType === InteractType.ACTIVATE) {
+            // 发动效果会多一个字段
+            dispatch(
+              addHandsIdleInteractivity({
+                player,
+                sequence: cardInfo.sequence,
+                interactivity: {
+                  interactType,
+                  activateIndex: data.effect_description,
+                  response: data.response,
+                },
+              })
+            );
+          } else if (interactType) {
+            dispatch(
+              addHandsIdleInteractivity({
+                player,
+                sequence: cardInfo.sequence,
+                interactivity: { interactType, response: data.response },
+              })
+            );
+          }
+
+          break;
+        }
+        default: {
         }
       }
+    });
+  });
+};
+
+function idleTypeToInteractType(
+  idleType: MsgSelectIdleCmd.IdleCmd.IdleType
+): InteractType | undefined {
+  switch (idleType) {
+    case MsgSelectIdleCmd.IdleCmd.IdleType.SUMMON: {
+      return InteractType.SUMMON;
+    }
+    case MsgSelectIdleCmd.IdleCmd.IdleType.SPSUMMON: {
+      return InteractType.SP_SUMMON;
+    }
+    case MsgSelectIdleCmd.IdleCmd.IdleType.POS_CHANGE: {
+      return InteractType.POS_CHANGE;
+    }
+    case MsgSelectIdleCmd.IdleCmd.IdleType.MSET: {
+      return InteractType.MSET;
+    }
+    case MsgSelectIdleCmd.IdleCmd.IdleType.SSET: {
+      return InteractType.SSET;
+    }
+    case MsgSelectIdleCmd.IdleCmd.IdleType.ACTIVATE: {
+      return InteractType.ACTIVATE;
+    }
+    default: {
+      return undefined;
     }
   }
-};
+}
