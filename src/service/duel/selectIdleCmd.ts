20a0a7384092495ec9974b359e6a2f62ba4b2e51
chechunchi <chechunchi.ccc@bytedance.com>
2023-01-15 16:38:21 +0800

    update selectIdleCmd service

--- src/service/duel/selectIdleCmd.ts
+++ src/service/duel/selectIdleCmd.ts
@@ -1,11 +1,12 @@
 import { ygopro } from "../../api/ocgcore/idl/ocgcore";
 import { AppDispatch } from "../../store";
-import { InteractType } from "../../reducers/duel/generic";
+import { Interactivity, InteractType } from "../../reducers/duel/generic";
 import {
   clearHandsIdleInteractivity,
   addHandsIdleInteractivity,
 } from "../../reducers/duel/mod";
 import MsgSelectIdleCmd = ygopro.StocGameMessage.MsgSelectIdleCmd;
+import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 
 export default (selectIdleCmd: MsgSelectIdleCmd, dispatch: AppDispatch) => {
   const player = selectIdleCmd.player;
@@ -14,6 +15,45 @@ export default (selectIdleCmd: MsgSelectIdleCmd, dispatch: AppDispatch) => {
   // 先清掉之前的手牌互动性
   dispatch(clearHandsIdleInteractivity(player));
 
+  const dispatcher = (
+    idleData: MsgSelectIdleCmd.IdleCmd.IdleData,
+    interactType: InteractType | undefined,
+    actionCreator: ActionCreatorWithPayload<
+      {
+        player: number;
+        sequence: number;
+        interactivity: Interactivity<number>;
+      },
+      string
+    >
+  ) => {
+    const cardInfo = idleData.card_info;
+    if (interactType === InteractType.ACTIVATE) {
+      // 发动效果会多一个字段
+      dispatch(
+        actionCreator({
+          player,
+          sequence: cardInfo.sequence,
+          interactivity: {
+            interactType,
+            activateIndex: idleData.effect_description,
+            response: idleData.response,
+          },
+        })
+      );
+    } else if (interactType) {
+      dispatch(
+        actionCreator({
+          player,
+          sequence: cardInfo.sequence,
+          interactivity: { interactType, response: idleData.response },
+        })
+      );
+    } else {
+      console.log(`InteractType undefined`);
+    }
+  };
+
   cmds.forEach((cmd) => {
     const interactType = idleTypeToInteractType(cmd.idle_type);
 
@@ -21,31 +61,11 @@ export default (selectIdleCmd: MsgSelectIdleCmd, dispatch: AppDispatch) => {
       const cardInfo = data.card_info;
       switch (cardInfo.location) {
         case ygopro.CardZone.HAND: {
-          if (interactType === InteractType.ACTIVATE) {
-            // 发动效果会多一个字段
-            dispatch(
-              addHandsIdleInteractivity({
-                player,
-                sequence: cardInfo.sequence,
-                interactivity: {
-                  interactType,
-                  activateIndex: data.effect_description,
-                  response: data.response,
-                },
-              })
-            );
-          } else if (interactType) {
-            dispatch(
-              addHandsIdleInteractivity({
-                player,
-                sequence: cardInfo.sequence,
-                interactivity: { interactType, response: data.response },
-              })
-            );
-          }
+          dispatcher(data, interactType, addHandsIdleInteractivity);
 
           break;
         }
+
         default: {
         }
       }
