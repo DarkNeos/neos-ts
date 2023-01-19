115db6d8f1ebac323601f2ef93c4519bd2d2bbca
chechunchi <chechunchi.ccc@bytedance.com>
2023-01-19 18:02:57 +0800

    add checkCardModalV2.tsx

---
+++ src/ui/Duel/checkCardModalV2.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { useAppSelector } from "../../hook";
+import { store } from "../../store";
+
+const CheckCardModalV2 = () => (<></>);
+
+export default CheckCardModalV2;
