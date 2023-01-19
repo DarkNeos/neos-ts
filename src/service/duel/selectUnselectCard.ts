0527f48b6b145fd4802bc16973333ae83cc16778
chechunchi <chechunchi.ccc@bytedance.com>
2023-01-19 20:27:17 +0800

    update select unselect card service

--- src/service/duel/selectUnselectCard.ts
+++ src/service/duel/selectUnselectCard.ts
@@ -1,4 +1,11 @@
 import { ygopro } from "../../api/ocgcore/idl/ocgcore";
+import {
+  setCheckCardModalV2CancelAble,
+  setCheckCardModalV2FinishAble,
+  setCheckCardModalV2IsOpen,
+  setCheckCardModalV2MinMax,
+} from "../../reducers/duel/mod";
+import { fetchCheckCardMetaV2 } from "../../reducers/duel/modal/checkCardModalV2Slice";
 import { AppDispatch } from "../../store";
 import MsgSelectUnselectCard = ygopro.StocGameMessage.MsgSelectUnselectCard;
 
@@ -6,5 +13,36 @@ export default (
   selectUnselectCard: MsgSelectUnselectCard,
   dispatch: AppDispatch
 ) => {
-  console.log(selectUnselectCard);
+  const controler = selectUnselectCard.player;
+  const finishable = selectUnselectCard.finishable;
+  const cancelable = selectUnselectCard.cancelable;
+  const min = selectUnselectCard.min;
+  const max = selectUnselectCard.max;
+  const selectableCards = selectUnselectCard.selectable_cards;
+  const selectedCards = selectUnselectCard.selected_cards;
+
+  dispatch(setCheckCardModalV2IsOpen(true));
+  dispatch(setCheckCardModalV2FinishAble(finishable));
+  dispatch(setCheckCardModalV2CancelAble(cancelable));
+  dispatch(setCheckCardModalV2MinMax({ min, max }));
+
+  selectableCards.forEach((card) => {
+    dispatch(
+      fetchCheckCardMetaV2({
+        controler,
+        selected: false,
+        option: { code: card.code, response: card.response },
+      })
+    );
+  });
+
+  selectedCards.forEach((card) => {
+    dispatch(
+      fetchCheckCardMetaV2({
+        controler,
+        selected: true,
+        option: { code: card.code },
+      })
+    );
+  });
 };
