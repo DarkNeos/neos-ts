0d0f0094b4d9f4dd08ae6dfeb24da40bfdfd6beb
SKTT1Ryze <linuxgnulover@gmail.com>
2023-01-23 17:20:02 +0800

    add select yes no service

---
+++ src/service/duel/selectYesNo.ts
@@ -0,0 +1,10 @@
+import { ygopro } from "../../api/ocgcore/idl/ocgcore";
+import { AppDispatch } from "../../store";
+import MsgSelectYesNo = ygopro.StocGameMessage.MsgSelectYesNo;
+
+export default (selectYesNo: MsgSelectYesNo, dispatch: AppDispatch) => {
+  const player = selectYesNo.player;
+  const effect_description = selectYesNo.effect_description;
+
+
+}
