5a2fc4c79f88dc54006f330c7ffd287655a80035
SKTT1Ryze <linuxgnulover@gmail.com>
2023-03-04 11:40:50 +0800

    remove unuse const

--- src/config/ui.ts
+++ src/config/ui.ts
@@ -11,22 +11,7 @@ export const GroundShape = () => {
 export const CardSlotShape = () => {
   return { width: 0.8, height: 1, depth: 0.05 };
 };
-export const DeckSlotShape = () => {
-  return { width: 0.8, height: 1, depth: 0.5 };
-};
-export const ExtraDeckSlotShape = () => {
-  return { width: 0.8, height: 1, depth: 0.2 };
-};
 export const SingleSlotShape = { width: 0.8, height: 1, depth: 0.2 };
-export const CemeterySlotShape = () => {
-  return { width: 0.8, height: 1, depth: 0.2 };
-};
-export const ExclusionSlotShape = () => {
-  return { width: 0.8, height: 1, depth: 0.2 };
-};
-export const FieldSlotShape = () => {
-  return { width: 0.8, height: 1, depth: 0.2 };
-};
 export const CardSlotRotation = (reverse: boolean) => {
   return reverse
     ? new BABYLON.Vector3(1.55, 3.1, 0)
@@ -34,18 +19,6 @@ export const CardSlotRotation = (reverse: boolean) => {
 };
 export const CardSlotDefenceRotation = () => {
   return new BABYLON.Vector3(1.55, 1.55, 0);
-};
-export const DeckSlotRotation = () => {
-  return new BABYLON.Vector3(1.55, 0, 0);
-};
-export const CemeterySlotRotation = () => {
-  return new BABYLON.Vector3(1.55, 0, 0);
-};
-export const ExclusionSlotRotation = () => {
-  return new BABYLON.Vector3(1.55, 0, 0);
-};
-export const FieldSlotRotation = () => {
-  return new BABYLON.Vector3(1.55, 0, 0);
 };
 
 // 浮空
@@ -58,16 +31,6 @@ export const HandShape = () => {
 export const HandRotation = () => {
   return new BABYLON.Vector3(1, 0, 0);
 };
-export const HandColor = () => {
-  return BABYLON.Color3.White();
-};
 export const HandHoverScaling = () => {
   return new BABYLON.Vector3(1.2, 1.2, 1);
 };
-export const HandHoverOutScaling = () => {
-  return new BABYLON.Vector3(1, 1, 1);
-};
-export const HandInteractShape = () => {
-  return { width: 0.8, height: 0.2 };
-};
-export const HandInteractFontSize = 200;
