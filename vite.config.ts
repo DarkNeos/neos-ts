4b39364b494976b5900fdb8882ef97ccb003950d
chechunchi <chechunchi.ccc@bytedance.com>
2023-03-14 19:41:06 +0800

    remove vite-plugin-wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), wasm()]
+  plugins: [react(), svgr()]
 })
