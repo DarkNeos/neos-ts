54e935ac4ea31ceaf5ac38f2ac6e7bfdb76dba39
SKTT1Ryze <linuxgnulover@gmail.com>
2023-03-15 00:08:53 +0800

    fix

--- src/api/ocgcore/ocgAdapter/stoc/stocGameMsg/damage.ts
+++ src/api/ocgcore/ocgAdapter/stoc/stocGameMsg/damage.ts
@@ -1,5 +1,6 @@
 import { ygopro } from "../../../idl/ocgcore";
-import init, { ocgDamageAdapter } from "rust-src";
+//@ts-ignore
+import { ocgDamageAdapter } from "rust-src";
 
 /*
  * Msg Damage
@@ -7,8 +8,8 @@ import init, { ocgDamageAdapter } from "rust-src";
  * @param player - 玩家编号
  * @param value - 减少的Hp数值
  * */
-export default async (data: Uint8Array) => {
-  const damage = await init().then(() => ocgDamageAdapter(data));
+export default (data: Uint8Array) => {
+  const damage = ocgDamageAdapter(data);
 
   return new ygopro.StocGameMessage.MsgUpdateHp(damage);
 };
