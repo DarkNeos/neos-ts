0f8047d9b74fe204c8dce687a1b1ad5e1c01137e
chechunchi <chechunchi.ccc@bytedance.com>
2023-03-14 20:09:11 +0800

    setup vite-plugin-wasm-pack

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr(), wasmPack('./rust-src')]
 })
