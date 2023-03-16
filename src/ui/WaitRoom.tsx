7cd64275b278aaac8a4e8e24cf5886b73de274ac
chechunchi <chechunchi.ccc@bytedance.com>
2023-03-16 14:22:25 +0800

    fix mycard deploy

--- src/ui/WaitRoom.tsx
+++ src/ui/WaitRoom.tsx
@@ -83,7 +83,7 @@ const WaitRoom = () => {
         await initStrings();
 
         // 初始化wasm
-        await rustInit(new URL("rust_src_bg.wasm", import.meta.url));
+        await rustInit(new URL("rust_src_bg.wasm", import.meta.env.BASE_URL));
       };
 
       init();
