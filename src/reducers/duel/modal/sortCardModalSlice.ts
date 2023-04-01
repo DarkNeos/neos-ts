7dfeceda6a82e374fde5e8f4069f19c21292a7c0
SKTT1Ryze <ccc@neos.moe>
2023-04-01 11:46:29 +0800

    add sortCardModalSlice

---
+++ src/reducers/duel/modal/sortCardModalSlice.ts
@@ -0,0 +1,48 @@
+import {
+  ActionReducerMapBuilder,
+  CaseReducer,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+import { fetchCard } from "../../../api/cards";
+import { ygopro } from "../../../api/ocgcore/idl/ocgcore";
+import { RootState } from "../../../store";
+import { DuelReducer } from "../generic";
+import { DuelState } from "../mod";
+type SortCard = ReturnType<
+  typeof ygopro.StocGameMessage.MsgSortCard.Info.prototype.toObject
+>;
+
+export const setSortCardModalIsOpenImpl: DuelReducer<boolean> = (
+  state,
+  action
+) => {
+  state.modalState.sortCardModal.isOpen = action.payload;
+};
+
+export const resetSortCardModalImpl: CaseReducer<DuelState> = (state) => {
+  state.modalState.sortCardModal.isOpen = false;
+  state.modalState.sortCardModal.options = [];
+};
+
+export const fetchSortCardMeta = createAsyncThunk(
+  "duel/fetchSortCardMeta",
+  async (param: SortCard) => {
+    const meta = await fetchCard(param.code!, true);
+    return {
+      meta,
+      response: param.response!,
+    };
+  }
+);
+
+export const sortCardModalCase = (
+  builder: ActionReducerMapBuilder<DuelState>
+) => {
+  // 这里更合理的做法是`pending`的时候先更新`options`，等`meta`数据返回后再异步更新`meta`
+  builder.addCase(fetchSortCardMeta.fulfilled, (state, action) => {
+    state.modalState.sortCardModal.options.push(action.payload);
+  });
+};
+
+export const selectSortCardModal = (state: RootState) =>
+  state.duel.modalState.sortCardModal;
--- src/service/duel/sortCard.ts
+++ src/service/duel/sortCard.ts
@@ -1,7 +1,10 @@
 import { ygopro } from "../../api/ocgcore/idl/ocgcore";
+import { fetchSortCardMeta } from "../../reducers/duel/modal/sortCardModalSlice";
 import { AppDispatch } from "../../store";
 import MsgSortCard = ygopro.StocGameMessage.MsgSortCard;
 
 export default (sortCard: MsgSortCard, dispatch: AppDispatch) => {
-  console.log(sortCard);
+  for (const option of sortCard.options) {
+    dispatch(fetchSortCardMeta(option.toObject()));
+  }
 };
