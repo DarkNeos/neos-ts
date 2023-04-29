7758ffcc378a907f8378b0e27eaf4b2925460763
SKTT1Ryze <ccc@neos.moe>
2023-04-29 13:21:52 +0800

    use new mat

--- src/ui/Duel/PlayMat2/index.ts
+++ src/ui/Duel/PlayMat2/index.ts
@@ -1,1 +1,3 @@
-export * from "./Mat";
+import { Mat } from "./Mat";
+
+export default Mat;
--- src/ui/Neos.tsx
+++ src/ui/Neos.tsx
@@ -7,6 +7,7 @@ const Login = React.lazy(() => import("./Login"));
 const WaitRoom = React.lazy(() => import("./WaitRoom"));
 const Mora = React.lazy(() => import("./Mora"));
 const NeosDuel = React.lazy(() => import("./Duel/Main"));
+const Mat = React.lazy(() => import("./Duel/PlayMat2"));
 
 export default function () {
   return (
@@ -32,7 +33,7 @@ export default function () {
         path="/duel/:player/:passWd/:ip"
         element={
           <Suspense fallback={<Loading />}>
-            <NeosDuel />
+            <Mat />
           </Suspense>
         }
       />
