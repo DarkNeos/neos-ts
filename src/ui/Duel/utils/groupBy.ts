35225f7c2cb695cf9cd0cb76305124a723fb386f
chechunchi <chechunchi.ccc@bytedance.com>
2023-05-12 15:46:06 +0800

    group selectab actions

---
+++ src/ui/Duel/utils/groupBy.ts
@@ -0,0 +1,13 @@
+export const groupBy = <K, V>(
+  array: readonly V[],
+  getKey: (cur: V, idx: number, src: readonly V[]) => K
+): [K, V[]][] =>
+  Array.from(
+    array.reduce((map, cur, idx, src) => {
+      const key = getKey(cur, idx, src);
+      const list = map.get(key);
+      if (list) list.push(cur);
+      else map.set(key, [cur]);
+      return map;
+    }, new Map<K, V[]>())
+  );
