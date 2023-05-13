a77c4f12e875755e2cf68f1960fbc04484868f91
SKTT1Ryze <ccc@neos.moe>
2023-05-13 13:15:39 +0800

    fix timelimit

--- src/service/duel/timeLimit.ts
+++ src/service/duel/timeLimit.ts
@@ -1,11 +1,7 @@
 import { sendTimeConfirm, ygopro } from "@/api";
 import { matStore } from "@/stores";
 
-const TIME_GAP = 200; // TODO: use config
-
 export default function handleTimeLimit(timeLimit: ygopro.StocTimeLimit) {
-  setTimeout(() => {
-    matStore.timeLimits.set(timeLimit.player, timeLimit.left_time);
-    sendTimeConfirm();
-  }, TIME_GAP);
+  matStore.timeLimits.set(timeLimit.player, timeLimit.left_time);
+  sendTimeConfirm();
 }
