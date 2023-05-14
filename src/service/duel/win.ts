1ad111179eb451e40df5063b6fc0696b95e96904
SKTT1Ryze <ccc@neos.moe>
2023-05-14 15:48:22 +0800

    fix win

--- src/service/duel/win.ts
+++ src/service/duel/win.ts
@@ -1,6 +1,15 @@
 import { ygopro } from "@/api";
 import { matStore } from "@/stores";
+import MsgWin = ygopro.StocGameMessage.MsgWin;
 
-export default (win: ygopro.StocGameMessage.MsgWin) => {
-  matStore.result = win.type_;
+export default (win: MsgWin) => {
+  const player = win.player;
+  if (matStore.isMe(player)) {
+    matStore.result = win.type_;
+  } else {
+    matStore.result =
+      win.type_ == MsgWin.ActionType.Win
+        ? MsgWin.ActionType.Defeated
+        : MsgWin.ActionType.Win;
+  }
 };
