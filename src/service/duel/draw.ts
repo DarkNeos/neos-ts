eb51fd1028ecef7a65e9888d3074245e598a02f7
SKTT1Ryze <ccc@neos.moe>
2023-05-19 23:47:26 +0800

    fix small

--- src/service/duel/draw.ts
+++ src/service/duel/draw.ts
@@ -1,8 +1,9 @@
 import { ygopro } from "@/api";
+import { sleep } from "@/infra";
 import { fetchEsHintMeta, matStore } from "@/stores";
 import { zip } from "@/ui/Duel/utils";
 
-export default (draw: ygopro.StocGameMessage.MsgDraw) => {
+export default async (draw: ygopro.StocGameMessage.MsgDraw) => {
   fetchEsHintMeta({ originMsg: "玩家抽卡时" });
 
   const deckLength = matStore.decks.of(draw.player).length;
@@ -19,9 +20,9 @@ export default (draw: ygopro.StocGameMessage.MsgDraw) => {
     .of(draw.player)
     .add(data, ygopro.CardPosition.FACEUP_ATTACK, true);
 
-  setTimeout(() => {
-    for (const hand of matStore.hands.of(draw.player)) {
-      hand.focus = false;
-    }
-  }, 500);
+  await sleep(500);
+
+  for (const hand of matStore.hands.of(draw.player)) {
+    hand.focus = false;
+  }
 };
