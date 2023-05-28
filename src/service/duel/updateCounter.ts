3e9be707a63dd051be59f13da21f3571ed1a9d5a
SKTT1Ryze <ccc@neos.moe>
2023-05-28 09:18:23 +0800

    fix updateCounter

--- src/service/duel/updateCounter.ts
+++ src/service/duel/updateCounter.ts
@@ -1,11 +1,12 @@
 import { ygopro } from "@/api";
-import { getCardByLocation } from "@/stores";
+import { cardStore } from "@/stores";
 
 type MsgUpdateCounter = ygopro.StocGameMessage.MsgUpdateCounter;
 
 export default (updateCounter: MsgUpdateCounter) => {
   const { location, count, action_type: counterType } = updateCounter;
-  const target = getCardByLocation(location); // 不太确定这个后面能不能相应，我不好说
+
+  const target = cardStore.find(location); // 不太确定这个后面能不能相应，我不好说
   if (target) {
     switch (counterType) {
       case ygopro.StocGameMessage.MsgUpdateCounter.ActionType.ADD: {
