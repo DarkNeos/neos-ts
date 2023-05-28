24a8038d5cff3071f872858fde30547ba633051f
SKTT1Ryze <ccc@neos.moe>
2023-05-28 17:17:56 +0800

    fix shuffle hand

--- src/service/duel/shuffleHand.ts
+++ src/service/duel/shuffleHand.ts
@@ -8,25 +8,33 @@ export default (shuffleHand: MsgShuffleHand) => {
 
   // 本质上是要将手卡的sequence变成和codes一样的顺序
   const hands = cardStore.at(ygopro.CardZone.HAND, controller);
-  const t: Record<number, number[]> = {};
+  const t: Map<number, number[]> = new Map([]);
   codes.forEach((code, sequence) => {
-    t[code] = t[code] || [];
-    t[code].push(sequence);
+    const v = t.get(code);
+    if (v) {
+      v.push(sequence);
+    } else {
+      t.set(code, [sequence]);
+    }
   });
+
+  console.log(t);
   hands.forEach((hand) => {
-    const target = t[hand.code];
+    const target = t.get(hand.code);
     if (target !== undefined) {
       const sequence = target.shift();
       if (sequence) {
         hand.sequence = sequence;
       } else {
         console.warn(
-          `<ShuffleHand>sequence shift from target is null, controller=${controller} hands=${hands}, codes=${codes}`
+          `<ShuffleHand>sequence shift from target is null, controller=${controller}, target=${target}, codes=${codes}`
         );
       }
     } else {
       console.warn(
-        `<ShuffleHand>target from records is null, controller=${controller} hands=${hands}, codes=${codes}`
+        `<ShuffleHand>target from records is null, controller=${controller}, hands=${hands.map(
+          (hand) => hand.code
+        )}, codes=${codes}`
       );
     }
   });
