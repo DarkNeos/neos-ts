8613b7ec34aded25361530eef5a226ed43992c18
SKTT1Ryze <ccc@neos.moe>
2023-06-11 18:09:05 +0800

    update small

--- src/service/duel/becomeTarget.ts
+++ src/service/duel/becomeTarget.ts
@@ -1,5 +1,18 @@
 import { ygopro } from "@/api";
+import { cardStore } from "@/stores";
 
 export default (becomeTarget: ygopro.StocGameMessage.MsgBecomeTarget) => {
-  console.log(becomeTarget);
+  for (const location of becomeTarget.locations) {
+    const target = cardStore.at(
+      location.zone,
+      location.controller,
+      location.sequence
+    );
+    if (target) {
+      console.info(`${target.meta.text.name} become target`);
+      // TODO: 动画
+    } else {
+      console.warn(`<BecomeTarget>target from ${location} is null`);
+    }
+  }
 };
