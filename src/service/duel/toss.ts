277c93c4d2c5bbfc72149de3f9c12031c6d12b05
SKTT1Ryze <ccc@neos.moe>
2023-06-17 12:10:51 +0800

    fix

--- src/service/duel/toss.ts
+++ src/service/duel/toss.ts
@@ -1,18 +1,27 @@
 import { fetchStrings, ygopro } from "@/api";
+import { sleep } from "@/infra";
 import { matStore } from "@/stores";
 import MsgToss = ygopro.StocGameMessage.MsgToss;
 
-export default (toss: MsgToss) => {
+export default async (toss: MsgToss) => {
+  const player = toss.player;
   const tossType = toss.toss_type;
+
+  const prefix = fetchStrings("!system", matStore.isMe(player) ? 102 : 103);
 
   for (const x of toss.res) {
     if (tossType == MsgToss.TossType.DICE) {
-      matStore.tossResult = fetchStrings("!system", 1624) + x;
+      matStore.tossResult = prefix + fetchStrings("!system", 1624) + x;
     } else if (tossType == MsgToss.TossType.COIN) {
       matStore.tossResult =
-        fetchStrings("!system", 1623) + fetchStrings("!system", 60 + x);
+        prefix +
+        fetchStrings("!system", 1623) +
+        fetchStrings("!system", 61 - x);
     } else {
       console.log(`Unknown tossType = ${tossType}`);
     }
+
+    // 等待1s，不然多个结果刷新太快了
+    await sleep(1000);
   }
 };
