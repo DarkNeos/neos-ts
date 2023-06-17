619b22263ebe5c6565eeac6677f6281e6db51410
SKTT1Ryze <ccc@neos.moe>
2023-06-17 11:47:37 +0800

    update toss.ts

--- src/service/duel/toss.ts
+++ src/service/duel/toss.ts
@@ -2,5 +2,19 @@ import { ygopro } from "@/api";
 import MsgToss = ygopro.StocGameMessage.MsgToss;
 
 export default (toss: MsgToss) => {
-  console.log(toss);
+  const tossType = toss.toss_type;
+
+  for (const x of toss.res) {
+    if (tossType == MsgToss.TossType.DICE) {
+      console.log(`骰子结果：${x}`);
+    } else if (tossType == MsgToss.TossType.COIN) {
+      if (x) {
+        console.log(`硬币正面`);
+      } else {
+        console.log(`硬币反面`);
+      }
+    } else {
+      console.log(`Unknown tossType = ${tossType}`);
+    }
+  }
 };
