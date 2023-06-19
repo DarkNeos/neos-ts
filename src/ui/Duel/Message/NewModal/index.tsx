affcf75493f4949f87731d7b815b1bc3728c026d
timelic <i@timel.ink>
2023-06-19 18:17:48 +0800

    feat: update style

--- src/ui/Duel/Message/NewModal/index.tsx
+++ src/ui/Duel/Message/NewModal/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Modal, type ModalProps } from "antd";
 import { type FC, useRef, useState, type CSSProperties } from "react";
-import { MinusOutlined } from "@ant-design/icons";
+import { MinusOutlined, UpOutlined } from "@ant-design/icons";
 import classNames from "classnames";
 import "./index.scss";
 
@@ -8,16 +8,18 @@ interface Props extends ModalProps {
   canBeMinimized?: boolean;
 }
 
-export const NewModal: FC<Props> = (props) => {
+export const NeosModal: FC<Props> = (props) => {
   const { canBeMinimized = true } = props;
   const [mini, setMini] = useState(false);
   return (
     <Modal
       className={classNames("neos-modal", { "neos-modal-mini": mini })}
       centered
+      maskClosable={false}
       onCancel={() => setMini(!mini)}
-      closeIcon={<MinusOutlined />}
+      closeIcon={mini ? <UpOutlined /> : <MinusOutlined />}
       bodyStyle={{ padding: "10px 0" }}
+      mask={!mini}
       {...props}
     />
   );
