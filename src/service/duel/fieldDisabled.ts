662ac6789c24e9d25f1df10f0814f5f869223f3a
chechunchi <chechunchi.ccc@bytedance.com>
2023-06-21 13:00:48 +0800

    update fieldDisabled.ts

--- src/service/duel/fieldDisabled.ts
+++ src/service/duel/fieldDisabled.ts
@@ -1,5 +1,19 @@
 import { ygopro } from "@/api";
+import { placeStore } from "@/stores";
 import MsgFieldDisabled = ygopro.StocGameMessage.MsgFieldDisabled;
+
 export default (fieldDisabled: MsgFieldDisabled) => {
-  console.log(fieldDisabled);
+  for (const action of fieldDisabled.actions) {
+    switch (action.zone) {
+      case ygopro.CardZone.MZONE:
+      case ygopro.CardZone.SZONE:
+        placeStore.set(action.zone, action.controller, action.sequence, {
+          interactivity: undefined,
+          disabled: true,
+        });
+        break;
+      default:
+        console.warn("<FieldDisabled>zone is not MZONE nor SZONE!");
+    }
+  }
 };
