4792ca5d9b09eb05656831778c29c18759f409a4
timelic <i@timel.ink>
2023-06-26 00:51:11 +0800

    feat: life bar using spring

--- src/ui/Duel/PlayMat/LifeBar/index.tsx
+++ src/ui/Duel/PlayMat/LifeBar/index.tsx
@@ -1,15 +1,33 @@
 import "./index.scss";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useSnapshot } from "valtio";
 
 import { matStore, playerStore } from "@/stores";
 import classNames from "classnames";
 
+import AnimatedNumbers from "react-animated-numbers";
+
+// 三个候选方案
+// https://snack.expo.dev/?platform=web
+// https://github.com/heyman333/react-animated-numbers
+// https://www.npmjs.com/package/react-countup?activeTab=dependents
+
 export const LifeBar: React.FC = () => {
   const snap = useSnapshot(matStore.initInfo);
   const snapPlayer = useSnapshot(playerStore);
   const { currentPlayer } = useSnapshot(matStore);
+
+  const [meLife, setMeLife] = React.useState(0);
+  const [opLife, setOpLife] = React.useState(0);
+
+  useEffect(() => {
+    setMeLife(snap.me.life);
+  }, [snap.me.life]);
+
+  useEffect(() => {
+    setOpLife(snap.op.life);
+  }, [snap.op.life]);
 
   return (
     <div id="life-bar-container">
@@ -19,7 +37,9 @@ export const LifeBar: React.FC = () => {
         })}
       >
         <div className="name">{snapPlayer.getOpPlayer().name}</div>
-        <div className="life">{snap.op.life}</div>
+        <div className="life">
+          {<AnimatedNumbers animateToNumber={opLife} />}
+        </div>
       </div>
       <div
         className={classNames("life-bar", {
@@ -27,7 +47,9 @@ export const LifeBar: React.FC = () => {
         })}
       >
         <div className="name">{snapPlayer.getMePlayer().name}</div>
-        <div className="life">{snap.me.life}</div>
+        <div className="life">
+          <AnimatedNumbers animateToNumber={meLife} />
+        </div>
       </div>
     </div>
   );
