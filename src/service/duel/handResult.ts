7de528a25d02bbd702a04e8e031f65b2e021e7bb
chechunchi <chechunchi.ccc@bytedance.com>
2023-06-30 12:47:47 +0800

    handle MsgHandResult

--- src/service/duel/handResult.ts
+++ src/service/duel/handResult.ts
@@ -1,7 +1,10 @@
 import { ygopro } from "@/api";
+import { matStore } from "@/stores";
 import MsgHandResult = ygopro.StocGameMessage.MsgHandResult;
 
 export default (res: MsgHandResult) => {
-  console.log(res);
-  // TODO
+  const { result1, result2 } = res;
+
+  matStore.handResults.set(0, result1);
+  matStore.handResults.set(1, result2);
 };
