f0dd524859c76a24bc74ebd7cbcba3d198636ecf
chechunchi <chechunchi.ccc@bytedance.com>
2023-06-30 11:46:21 +0800

    handle RockPaperScissors

--- src/service/duel/rockPaperScissors.ts
+++ src/service/duel/rockPaperScissors.ts
@@ -1,6 +1,13 @@
 import { ygopro } from "@/api";
+import { displayOptionModal } from "@/ui/Duel/Message";
 
-export default (mora: ygopro.StocGameMessage.MsgRockPaperScissors) => {
-  console.log(mora);
-  // TODO
+export default async (mora: ygopro.StocGameMessage.MsgRockPaperScissors) => {
+  const _player = mora.player;
+
+  // TODO: I18n
+  await displayOptionModal("请选择猜拳", [
+    { msg: "剪刀", response: 1 },
+    { msg: "石头", response: 2 },
+    { msg: "布", response: 3 },
+  ]);
 };
