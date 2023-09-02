1f23b2d3e9973078d4eb009363e4af6c80b660be
SKTT1Ryze <ccc@neos.moe>
2023-09-02 14:14:40 +0800

    update small

--- src/service/utils/genCard.ts
+++ src/service/utils/genCard.ts
@@ -9,7 +9,7 @@ import { CardType } from "@/stores";
 // TODO: 其实不是很推荐这样做，因为随着项目复杂度增加，这样可能会带来meta更新的时序问题
 export const genCard = (card: CardType) => {
   const t = proxy(card);
-  subscribeKey(t, "code", (code) => {
+  subscribeKey(t, "code", async (code) => {
     const meta = fetchCard(code);
     t.meta = meta;
   });
