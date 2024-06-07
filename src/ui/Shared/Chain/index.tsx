f2f8c8d832d374ab530db7d2c20007a4ef364586
timelic <i@timel.ink>
2024-06-07 16:31:37 +0800

    fix: small

--- src/ui/Shared/Chain/index.tsx
+++ src/ui/Shared/Chain/index.tsx
@@ -15,18 +15,14 @@ export interface ChainProps {
 /* 这里有个妥协的实现：墓地，除外区，额外卡组的连锁图标会被卡片遮挡，原因不明,
  * 因此这里暂时采取移动一个身位的方式进行解决。最好的解决方案应该是UI上连锁图标和
  * 场地解耦。 */
-export const BgChain: React.FC<ChainProps> = ({
-  chains,
-  nBelow: nCardsBelow = 1,
-  op,
-}) => (
+export const BgChain: React.FC<ChainProps> = ({ chains, nBelow = 1, op }) => (
   <div
     className={classnames(styles.container, {
       [styles.op]: op,
     })}
     style={{
       // @ts-ignore
-      "--n": nCardsBelow,
+      "--n": nBelow,
     }}
   >
     {/* 暂时只适配最后的连锁，不然肯定会出现错位 */}
