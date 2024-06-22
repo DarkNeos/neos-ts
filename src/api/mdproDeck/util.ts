90240ae27ceaed8930d1e344c6666c9458109b67
SKTT1Ryze <ccc@neos.moe>
2024-06-22 11:27:35 +0800

    add ClientSource in mdpro deck header

--- src/api/mdproDeck/util.ts
+++ src/api/mdproDeck/util.ts
@@ -1,6 +1,7 @@
 export function mdproHeaders(): Headers {
   const myHeaders = new Headers();
   myHeaders.append("ReqSource", "MDPro3");
+  myHeaders.append("ClientSource", "Web");
 
   return myHeaders;
 }
